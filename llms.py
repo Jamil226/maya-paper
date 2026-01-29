c9a1e2bf13b03974dc1f5eaea89bf9ac6cfff667
Jamil226 <jamil138.amin@gmail.com>
2026-01-29 10:59:04 +0300

    update

--- llms.py
+++ llms.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatOllama(model='llama3', temperature=0)
+llm = ChatOllama(model='llama3.1', temperature=0)
 
-embeddings = OllamaEmbeddings(model='llama3')
+embeddings = OllamaEmbeddings(model='llama3.1')
